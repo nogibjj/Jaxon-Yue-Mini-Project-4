b21d58f4e4fe8861f85b368da727c46e15a90926
jaxonyue <70416390+jaxonyue@users.noreply.github.com>
2023-09-22 00:01:35 +0000

    update

--- test_main.py
+++ test_main.py
@@ -1,10 +1,5 @@
-from main import median, rows, columns
-
-def test_main():
-    assert median == 50564.5
-    assert rows == 38
-    assert columns == 5
+from main import add
 
 
-if __name__ == "__main__":
-    test_main()
+def test_add():
+    assert add(1, 2) == 3
